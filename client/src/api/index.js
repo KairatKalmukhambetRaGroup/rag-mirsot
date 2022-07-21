f5a7361b542c8d5c1daf641322911768ac121a87
qashqyn <qashqyn291@gmail.com>
2022-07-21 22:52:24 +0600

    test deploy

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/', validateStatus: function (status) { return true } });
+// const API = axios.create({ baseURL: 'http://localhost:5000/', validateStatus: function (status) { return true } });
+const API = axios.create({ baseURL: 'http://89.219.32.45:5000/', validateStatus: function (status) { return true } });
+http://89.219.32.45/
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')) {
